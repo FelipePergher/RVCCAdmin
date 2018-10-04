e4dd46582b4c9555fe6a7d1ebd0dac3011ede4fd
FelipePergher <felipepergher_10@hotmail.com>
2018-10-04 01:02:27 -0300

    Implementing crud of patient

--- _source/LigaCancer/Models/MedicalViewModels/PatientViewModel.cs
+++ _source/LigaCancer/Models/MedicalViewModels/PatientViewModel.cs
@@ -1,18 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LigaCancer.Data.Models.PatientModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using static LigaCancer.Code.Globals;
 
 namespace LigaCancer.Models.MedicalViewModels
 {
     public class PatientViewModel
     {
+        public PatientViewModel()
+        {
+            DateOfBirth = DateTime.Now;
+        }
+
         public int PatientId { get; set; }
 
-        [Display(Name = "Nome"), Required(ErrorMessage = "Este campo é obrigatório!"),
-            Remote("IsNameExist", "Patient", AdditionalFields = "PatientId", ErrorMessage = "Remédio já registrado!")]
-        public string Name { get; set; }
+        [Display(Name = "Nome"), Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Surname"), Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string RG { get; set; }
+
+        [Required]
+        public string CPF { get; set; }
+
+        [Display(Name = "Grupo de Convivência")]
+        public bool FamiliarityGroup { get; set; }
+
+        [Display(Name = "Sexo")]
+        public Sex Sex { get; set; }
+
+        [Display(Name = "Estado Civil")]
+        public CivilState CivilState { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        public NaturalityViewModel Naturality { get; set; }
+
+        public FamilyViewModel Family { get; set; }
+
+        public PatientInformationViewModel PatientInformation { get; set; }
+
+        public ICollection<PhoneViewModel> Phones { get; set; }
+
+        public ICollection<AddressViewModel> Addresses { get; set; }
+
+        public ICollection<AttachmentsViewModel> Attachments { get; set; }
+
     }
 }
