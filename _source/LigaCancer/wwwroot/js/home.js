bd7b1e2cb3c2261082590005e0b518391b2cda2b
Felipe Pergher <felipepergher_10@hotmail.com>
2019-01-08 01:17:03 -0200

    Updae client side libraries

--- _source/LigaCancer/wwwroot/js/home.js
+++ _source/LigaCancer/wwwroot/js/home.js
@@ -11,11 +11,11 @@
 
 /* eslint-disable no-magic-numbers */
 // Disable the on-canvas tooltip
-Chart.defaults.global.pointHitDetectionRadius = 1;
-Chart.defaults.global.tooltips.enabled = false;
-Chart.defaults.global.tooltips.mode = "index";
-Chart.defaults.global.tooltips.position = "nearest";
-Chart.defaults.global.tooltips.custom = CustomTooltips; // eslint-disable-next-line no-unused-vars
+//Chart.defaults.global.pointHitDetectionRadius = 1;
+//Chart.defaults.global.tooltips.enabled = false;
+//Chart.defaults.global.tooltips.mode = "index";
+//Chart.defaults.global.tooltips.position = "nearest";
+//Chart.defaults.global.tooltips.custom = CustomTooltips; // eslint-disable-next-line no-unused-vars
 
 //var cardChart1 = new Chart($('#card-chart1'), {
 //    type: 'line',
@@ -181,86 +181,86 @@ Chart.defaults.global.tooltips.custom = CustomTooltips; // eslint-disable-next-l
 //    }
 //}); // eslint-disable-next-line no-unused-vars
 
-var mainChart = new Chart($("#main-chart"), {
-    type: "line",
-    data: {
-        labels: ["M", "T", "W", "T", "F", "S", "S", "M", "T", "W", "T", "F", "S", "S", "M", "T", "W", "T", "F", "S", "S", "M", "T", "W", "T", "F", "S", "S"],
-        datasets: [{
-            label: "Exemplo 1",
-            backgroundColor: hexToRgba(getStyle("--info"), 10),
-            borderColor: getStyle("--info"),
-            pointHoverBackgroundColor: "#fff",
-            borderWidth: 2,
-            data: [165, 180, 70, 69, 77, 57, 125, 165, 172, 91, 173, 138, 155, 89, 50, 161, 65, 163, 160, 103, 114, 185, 125, 196, 183, 64, 137, 95, 112, 175]
-        }, {
-            label: "Exemplo 2",
-            backgroundColor: "transparent",
-            borderColor: getStyle("--success"),
-            pointHoverBackgroundColor: "#fff",
-            borderWidth: 2,
-            data: [92, 97, 80, 100, 86, 97, 83, 98, 87, 98, 93, 83, 87, 98, 96, 84, 91, 97, 88, 86, 94, 86, 95, 91, 98, 91, 92, 80, 83, 82]
-        }, {
-            label: "Exemplo 3",
-            backgroundColor: "transparent",
-            borderColor: getStyle("--danger"),
-            pointHoverBackgroundColor: "#fff",
-            borderWidth: 1,
-            borderDash: [8, 5],
-            data: [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65]
-        }]
-    },
-    options: {
-        maintainAspectRatio: false,
-        legend: {
-            display: false
-        },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    drawOnChartArea: false
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    maxTicksLimit: 5,
-                    stepSize: Math.ceil(250 / 5),
-                    max: 250
-                }
-            }]
-        },
-        elements: {
-            point: {
-                radius: 0,
-                hitRadius: 10,
-                hoverRadius: 4,
-                hoverBorderWidth: 3
-            }
-        }
-    }
-});
-var brandBoxChartLabels = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho"];
-var brandBoxChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    legend: {
-        display: false
-    },
-    scales: {
-        xAxes: [{
-            display: false
-        }],
-        yAxes: [{
-            display: false
-        }]
-    },
-    elements: {
-        point: {
-            radius: 0,
-            hitRadius: 10,
-            hoverRadius: 4,
-            hoverBorderWidth: 3
-        }
-    } // eslint-disable-next-line no-unused-vars
+//var mainChart = new Chart($("#main-chart"), {
+//    type: "line",
+//    data: {
+//        labels: ["M", "T", "W", "T", "F", "S", "S", "M", "T", "W", "T", "F", "S", "S", "M", "T", "W", "T", "F", "S", "S", "M", "T", "W", "T", "F", "S", "S"],
+//        datasets: [{
+//            label: "Exemplo 1",
+//            backgroundColor: hexToRgba(getStyle("--info"), 10),
+//            borderColor: getStyle("--info"),
+//            pointHoverBackgroundColor: "#fff",
+//            borderWidth: 2,
+//            data: [165, 180, 70, 69, 77, 57, 125, 165, 172, 91, 173, 138, 155, 89, 50, 161, 65, 163, 160, 103, 114, 185, 125, 196, 183, 64, 137, 95, 112, 175]
+//        }, {
+//            label: "Exemplo 2",
+//            backgroundColor: "transparent",
+//            borderColor: getStyle("--success"),
+//            pointHoverBackgroundColor: "#fff",
+//            borderWidth: 2,
+//            data: [92, 97, 80, 100, 86, 97, 83, 98, 87, 98, 93, 83, 87, 98, 96, 84, 91, 97, 88, 86, 94, 86, 95, 91, 98, 91, 92, 80, 83, 82]
+//        }, {
+//            label: "Exemplo 3",
+//            backgroundColor: "transparent",
+//            borderColor: getStyle("--danger"),
+//            pointHoverBackgroundColor: "#fff",
+//            borderWidth: 1,
+//            borderDash: [8, 5],
+//            data: [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65]
+//        }]
+//    },
+//    options: {
+//        maintainAspectRatio: false,
+//        legend: {
+//            display: false
+//        },
+//        scales: {
+//            xAxes: [{
+//                gridLines: {
+//                    drawOnChartArea: false
+//                }
+//            }],
+//            yAxes: [{
+//                ticks: {
+//                    beginAtZero: true,
+//                    maxTicksLimit: 5,
+//                    stepSize: Math.ceil(250 / 5),
+//                    max: 250
+//                }
+//            }]
+//        },
+//        elements: {
+//            point: {
+//                radius: 0,
+//                hitRadius: 10,
+//                hoverRadius: 4,
+//                hoverBorderWidth: 3
+//            }
+//        }
+//    }
+//});
+//var brandBoxChartLabels = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho"];
+//var brandBoxChartOptions = {
+//    responsive: true,
+//    maintainAspectRatio: false,
+//    legend: {
+//        display: false
+//    },
+//    scales: {
+//        xAxes: [{
+//            display: false
+//        }],
+//        yAxes: [{
+//            display: false
+//        }]
+//    },
+//    elements: {
+//        point: {
+//            radius: 0,
+//            hitRadius: 10,
+//            hoverRadius: 4,
+//            hoverBorderWidth: 3
+//        }
+//    } // eslint-disable-next-line no-unused-vars
 
-};
+//};
