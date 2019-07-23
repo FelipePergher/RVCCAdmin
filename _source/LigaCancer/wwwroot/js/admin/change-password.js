41b2d10298f945b68babd40549ed121cd4bed110
Amanda Serafini <amandaserafini1@hotmail.com>
2019-07-22 21:56:00 -0300

    #111-Password eye

--- _source/LigaCancer/wwwroot/js/admin/change-password.js
+++ _source/LigaCancer/wwwroot/js/admin/change-password.js
@@ -21,4 +21,17 @@ function initPage() {
         }
     });
 
+    $(".eyePassword").click(function () {
+        let icon = $(this);
+        let inputPassword = icon.siblings("input");
+        if (icon.hasClass("fa-eye")) {
+            icon.removeClass("fa-eye").addClass("fa-eye-slash");
+            inputPassword.attr("type", "password");
+        } else {
+            icon.addClass("fa-eye").removeClass("fa-eye-slash");
+            inputPassword.attr("type", "text");
+        }
+    });
+
+
 }
