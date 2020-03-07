24aeb5f95dd6c6f05005007e01ab291645a7eee2
Felipe Pergher <felipepergher_10@hotmail.com>
2020-03-07 17:27:35 -0300

    #155 adjust csproj and remove not used things

--- _source/LigaCancer/wwwroot/js/main.js
+++ _source/LigaCancer/wwwroot/js/main.js
@@ -1,34 +1,4 @@
-﻿const swalWithBootstrapButtons = Swal.mixin({
-    confirmButtonClass: 'btn btn-success ml-2',
-    cancelButtonClass: 'btn btn-danger',
-    buttonsStyling: false
-});
-
-let language = {
-    "sEmptyTable": "Nenhum registro encontrado",
-    "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
-    "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
-    "sInfoFiltered": "(Filtrados de _MAX_ registros)",
-    "sInfoPostFix": "",
-    "sInfoThousands": ".",
-    "sLengthMenu": "_MENU_ resultados por página",
-    "sLoadingRecords": "Carregando...",
-    "sProcessing": "Processando...",
-    "sZeroRecords": "Nenhum registro encontrado",
-    "sSearch": "Pesquisar",
-    "oPaginate": {
-        "sNext": "Próximo",
-        "sPrevious": "Anterior",
-        "sFirst": "Primeiro",
-        "sLast": "Último"
-    },
-    "oAria": {
-        "sSortAscending": ": Ordenar colunas de forma ascendente",
-        "sSortDescending": ": Ordenar colunas de forma descendente"
-    }
-};
-
-function setupPhoneMaskOnField(selector) {
+﻿function setupPhoneMaskOnField(selector) {
     var inputElement = $(selector);
 
     setCorrectPhoneMask(inputElement);
@@ -50,68 +20,6 @@ let masks = {
     Price: '000.000.000.000.000,00',
     date: '99/99/9999'
 };
-
-function calendar(id) {
-    $("#" + id).datepicker({
-        format: "dd/mm/yyyy",
-        iconsLibrary: "fontawesome",
-        locale: "pt-br",
-        uiLibrary: "bootstrap4"
-    });
-    $('#' + id).mask(masks.date, { placeholder: "dd/mm/yyyy" });
-}
-
-function time(id) {
-    let timepicker = $("#" + id).timepicker({
-        uiLibrary: "bootstrap4",
-        modal: false,
-        header: false,
-        footer: false,
-        mode: '24hr'
-    });
-    $("#" + id).siblings(".input-group-append").find("i").removeClass("gj-icon").removeClass("clock").addClass("fa").addClass("fa-clock");
-
-    $("#" + id).click(function () {
-        $(".timepicker").is(":visible") ? timepicker.close() : timepicker.open();
-    });
-}
-
-//Modal Primary functions
-function openModal(url, title, callback = null) {
-    $("#modal-title").text(title);
-
-    $("#modalBody").load(url, function () {
-        if (callback !== null) callback();
-        hideSpinnerModal();
-    });
-
-    $("#modal-action").on("hidden.bs.modal", function (e) {
-        cleanModal();
-    });
-
-    $('#modal-action').on('shown.bs.modal', function () {
-        $(this).find('input,select').filter(':visible:first').trigger('focus');
-    });
-}
-
-function cleanModal() {
-    $("#modalBody").html("");
-    $("#modal-title").text("");
-    $("#modal-dialog").removeClass("modal-lg");
-    $("#modal-dialog").removeClass("modal-elg");
-    showSpinnerModal();
-    setTimeout(function () {
-        $(".swal2-confirm").focus();
-    }, 10);
-}
-
-function showSpinnerModal() {
-    $("#modalSpinner").show();
-}
-
-function hideSpinnerModal() {
-    $("#modalSpinner").hide();
-}
 
 //Secondary Modal Functions
 function openModalSecondary(url, title, callback = null) {
@@ -148,22 +56,3 @@ function showSpinnerModalSecondary() {
 function hideSpinnerModalSecondary() {
     $("#modalSpinnerSecondary").hide();
 }
-
-$(function () {
-    if (!!$.validator) {
-        $.validator.setDefaults({
-            highlight: function highlight(element) {
-                $(element).addClass('is-invalid').removeClass('is-valid');
-            },
-            // eslint-disable-next-line object-shorthand
-            unhighlight: function unhighlight(element) {
-                $(element).addClass('is-valid').removeClass('is-invalid');
-            },
-            errorElement: 'span',
-            errorPlacement: function errorPlacement(error, element) {
-                error.addClass('invalid-feedback');
-                element.prop('type') === 'checkbox' ? error.insertAfter(element.parent('label')) : error.insertAfter(element);
-            }
-        });
-    }
-});
